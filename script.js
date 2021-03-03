b3eb9a1ebefe2946770e30e55a4e41245f7ce70d
Your Name <you@example.com>
2021-03-03 13:43:41 +0400

    script.js

--- script.js
+++ script.js
@@ -7,56 +7,49 @@ let body = document.body;
 let url = window.location.toString();
 
 let getName = (url) => {
-let urlNom = url.split('=');
-console.log(urlNom);
-let name = urlNom[1];	
-	if(name == undefined) {
-		name = 'Margaritas202';
- 	}
- 	return getName(url);
- 	}
+    let urlMas = url.split('=');
+    let name = urlMas[1];
+    if(name == undefined) {
+        name = 'Margaritas202';
+     }
+     return name;
+}
 
 let name = getName(url);
-
-fetch(`https://api.github.com/users/${getName(url)}`)
-  .then(res => res.json())
-  .then(json => {
-		login = json.login;
+console.log('https://api.github.com/users/' + name);
+fetch('https://api.github.com/users/' + name)
+    .then(res => res.json())
+    .then(json => {
         avatar = json.avatar_url;
+        userName = json.name;
         description = json.bio;
- 	    url = json.url;
-
-let addLogin = () => {
-	let login = document.createElement('h1');
- 	  user.Login.innerHTML = login;
- 	  body.appendChild(user.Login);
-}
-
-let addAvatar = () => {
-	let userAvatar = document.createElement('img');
- 	let newString = document.createElement('br');
- 	userAvatar.src = avatar
- 	body.appendChild(newString);
- 	body.appendChild(userAvatar);
-}
-
-let addDescription = () => {
-    let userDescription = document.createElement('p');
- 	  userDescription.innerHTML = description;
- 	  body.appendChild(userDescription);
-}
-
-let addUrl = () => {
- 	  let userUrl = document.createElement('a');
- 	  let text = document.createTextNode('Profile');
-  	  userUrl.href = userUrl;
- 	  userUrl.appendChild(text);
- 	  body.appendChild(userUrl);
-}
- 	addLogin();
- 	addDescription();
- 	addAvatar();
- 	addUrl();
- })
-
-.catch(err => console.log(err + 'Информация о пользователе не доступна')); 
+        url = json.url;
+        let addName = () => {
+            let userName = document.createElement('h1');
+            userName.innerHTML = name;
+            body.appendChild(userName);
+        }
+        let addDescription = () => {
+            let userDescription = document.createElement('h2');
+            userDescription.innerHTML = description;
+            body.appendChild(userDescription);
+        }
+        let addAvatar = () => {
+            let userAvatar = document.createElement('img');
+            let newString = document.createElement('br');
+            userAvatar.src = avatar;
+            body.appendChild(userAvatar);
+            body.appendChild(newString);
+        }
+        let addUrl = () => {
+            let userUrl = document.createElement('a');
+            let text = document.createTextNode('profile');
+            userUrl.appendChild(text);
+            userUrl.href = 'https://github.com/' + name;
+            body.appendChild(userUrl);
+        }
+        addName();
+        addDescription();
+        addAvatar();
+        addUrl();
+    }).catch(err => alert(err + 'Информация о пользователе не доступна'));
